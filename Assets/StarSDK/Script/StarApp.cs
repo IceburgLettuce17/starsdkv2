64e3eba69435d64732a458dbab572408ee4013d4
🧊 Iceburg 🧊 <140565337+IceburgLettuce17@users.noreply.github.com>
2025-12-25 17:00:08 -0800

    johnb: you forgot abstract

--- Assets/StarSDK/Script/StarApp.cs
+++ Assets/StarSDK/Script/StarApp.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 
 
 
-public class StarApp : MonoBehaviour
+public abstract class StarApp : MonoBehaviour
 {
 	public const int MOUSEBUTTON_LEFT = 0;
 	public const int MOUSEBUTTON_RIGHT = 1;
