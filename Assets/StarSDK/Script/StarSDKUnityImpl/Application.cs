fc922885dfa66645ee801a9c0957773d3862df79
🧊 Iceburg 🧊 <140565337+IceburgLettuce17@users.noreply.github.com>
2025-12-29 16:36:08 -0800

    Start reimplementing unityengine.application

---
+++ Assets/StarSDK/Script/StarSDKUnityImpl/Application.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace UnityEngineMod
+{
+	public sealed class Application
+	{
+		private static bool s_debugLogInApplicationQuit = true;
+		
+		public static void Quit(int arg0)
+		{
+			// WRAPPER CALL - UnityEngine.Application.Quit(int arg0)
+			Application.Quit(arg0);
+			
+			if (s_debugLogInApplicationQuit)
+			{
+				Debug.Log("[Application] Quitted with exit-code " + arg0);
+			}
+		}
+		
+		public static void Quit()
+		{
+			Quit(1);
+		}
+	}
+}
