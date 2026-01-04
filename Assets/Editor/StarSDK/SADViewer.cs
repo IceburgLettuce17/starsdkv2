490d92944e4b7aec05570778e99f0aab72b47265
🧊 Iceburg 🧊 <140565337+IceburgLettuce17@users.noreply.github.com>
2026-01-03 22:10:57 -0300

    Start work on implementing descriptor viewer

---
+++ Assets/Editor/StarSDK/SADViewer.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+public class SADViewer : EditorWindow
+{
+	[MenuItem("StarSDK/AppDescriptor Viewer")]
+	private static void Init()
+	{
+       SADViewer editor = (SADViewer)GetWindow(typeof(SADViewer), false, "AppDescriptor Viewer");
+
+		// Require the editor window to be at least 300 pixels wide
+		Vector2 minSize = editor.minSize;
+		minSize.x = 230;
+		editor.minSize = minSize;
+    }
+}
