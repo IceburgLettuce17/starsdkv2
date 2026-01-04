21642cf5241f05f262d7d195f26edd71c7fcb55c
🧊 Iceburg 🧊 <140565337+IceburgLettuce17@users.noreply.github.com>
2026-01-03 22:31:24 -0300

    a

---
+++ Assets/Editor/StarSDK/SADImportWizard.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class SADImportWizard : ScriptableWizard
+{
+	StarAppDescriptor descr;
+	
+	private void OnEnable()
+	{
+
+	}
+
+	private void OnInspectorUpdate()
+	{
+	    if (Resources.FindObjectsOfTypeAll(typeof(SADViewer)).Length == 0)
+	    {
+			Close();
+	    }
+	}
+
+	protected override bool DrawWizardGUI()
+	{
+	    GUILayout.Label("Welcome to the Star Application Descriptor import wizard." + 
+		" This will import a new Descriptor from a script.", EditorStyles.wordWrappedLabel);
+	    EditorGUILayout.Separator();
+	    return base.DrawWizardGUI();
+	}
+
+	private void OnWizardCreate()
+	{
+	    if (Resources.FindObjectsOfTypeAll(typeof(SADViewer)).Length >= 1)
+	    {
+			//((SADViewer)Resources.FindObjectsOfTypeAll(typeof(SADViewer))[0]).Import(importCompanyName, importProductName);
+	    }
+	}
+}
--- Assets/Editor/StarSDK/SADViewer.cs
+++ Assets/Editor/StarSDK/SADViewer.cs
@@ -3,14 +3,43 @@ using UnityEngine;
 using UnityEditor;
 public class SADViewer : EditorWindow
 {
+	private bool showAdd;
+	
+	private const int VIEW = 0;
+	
+	public StarAppDescriptor currentDescriptor;
+	
 	[MenuItem("StarSDK/AppDescriptor Viewer")]
 	private static void Init()
 	{
-       SADViewer editor = (SADViewer)GetWindow(typeof(SADViewer), false, "AppDescriptor Viewer");
+		SADViewer editor = (SADViewer)GetWindow(typeof(SADViewer), false, "AppDescriptor Viewer");
 
-		// Require the editor window to be at least 300 pixels wide
 		Vector2 minSize = editor.minSize;
 		minSize.x = 230;
 		editor.minSize = minSize;
     }
+	
+	private void OnGUI()
+	{
+		EditorGUILayout.Space();
+
+		DrawViewerTop();
+		
+		DrawViewMenu();
+	}
+	
+	private void DrawViewerTop()
+	{
+        GUILayout.Toolbar(VIEW, new string[] { "View Descriptors"});
+	}
+	
+	private void DrawViewMenu()
+	{
+		 if (GUILayout.Button("Import"))
+		 {
+			 //SADImportWizard wizard = ScriptableWizard.DisplayWizard<SADImportWizard>("Import a descriptor", "Import");
+			 GUILayout.Label("Please select a script of type " + "StarAppDescriptor.");
+			 
+		 }
+	}
 }
